3028ba465086701abb9dbdcd2b9cdde8ca59d8a8
Vladisalex <vl4disalex@gmail.com>
2026-09-25 19:44:41 +0300

    feat: add featured properties slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,11 @@
 import '../styles/main.scss'
 
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+
 import Header from './Header.js'
+import SliderCollection from './Slider.js'
 
 new Header()
+new SliderCollection()
